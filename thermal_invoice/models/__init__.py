bb410aa305a671a614fa87fc9940eb449b457a9c
valisoa-ramilijaona <valisoaramilijaona@gmail.com>
2021-11-22 15:14:47 +0100

    add thermal_invoice

---
+++ thermal_invoice/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- encoding: utf-8 -*-
+# This module and its content is copyright of Technaureus Info Solutions Pvt. Ltd.
+# - © Technaureus Info Solutions Pvt. Ltd 2019. All rights reserved.
+
+from . import report_data
