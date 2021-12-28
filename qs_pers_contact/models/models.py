0b517d56921f998e366732aba223a4d73c414cb3
AlphaConseils <alpha@alphamada.com>
2021-12-28 13:31:58 +0100

    NDA update

--- qs_pers_contact/models/models.py
+++ qs_pers_contact/models/models.py
@@ -9,5 +9,12 @@ class ResPartnerS2PC(models.Model):
     RCS = fields.Char('RCS', index=1)
     CIF = fields.Char('CIF', index=1)
 
+    @api.model
     def _default_terms(self):
-        return self.env['account.payment.term'].search([('id', '=', '1')], limit=1).id
+        return self.env['account.payment.term'].search([('name', '=', 'IMMEDIAT')], limit=1).id
+
+    property_payment_term_id = fields.Many2one('account.payment.term', company_dependent=True,
+                                               string='Customer Payment Terms',
+                                               domain="[('company_id', 'in', [current_company_id, False])]",
+                                               help="This payment term will be used instead of the default one for sales orders and customer invoices",
+                                               default=_default_terms)
