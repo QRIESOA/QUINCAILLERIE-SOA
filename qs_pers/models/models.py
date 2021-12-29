45dc82f95066def06bae38184293ebcd553a2de8
AlphaConseils <alpha@alphamada.com>
2021-12-29 15:59:46 +0100

    security update

--- qs_pers/models/models.py
+++ qs_pers/models/models.py
@@ -70,41 +70,17 @@ class qs_pers(models.Model):
             if self.credit_sale_limit < self.partner_id.total_due + self.amount_total:
                 raise UserError("Ce client doit payer son credit")
 
-    def _action_confirm_qs(self):
+    def _action_confirm(self):
         """ Implementation of additionnal mecanism of Sales Order confirmation.
             This method should be extended when the confirmation should generated
             other documents. In this method, the SO are in 'sale' state (not yet 'done').
         """
         self.check_credit_limit()
-        # create an analytic account if at least an expense product
-        for order in self:
-            if any(expense_policy not in [False, 'no'] for expense_policy in
-                   order.order_line.mapped('product_id.expense_policy')):
-                if not order.analytic_account_id:
-                    order._create_analytic_account()
+        return super(qs_pers, self)._action_confirm()
 
-        return True
-
-    def action_confirm_qs(self):
+    def action_confirm(self):
         self.check_credit_limit()
-        if self._get_forbidden_state_confirm() & set(self.mapped('state')):
-            raise UserError(_(
-                'It is not allowed to confirm an order in the following states: %s'
-            ) % (', '.join(self._get_forbidden_state_confirm())))
-
-        for order in self.filtered(lambda order: order.partner_id not in order.message_partner_ids):
-            order.message_subscribe([order.partner_id.id])
-        self.write(self._prepare_confirmation_values())
-
-        # Context key 'default_name' is sometimes propagated up to here.
-        # We don't need it and it creates issues in the creation of linked records.
-        context = self._context.copy()
-        context.pop('default_name', None)
-
-        self.with_context(context)._action_confirm_qs()
-        if self.env.user.has_group('sale.group_auto_done_setting'):
-            self.action_done()
-        return True
+        return super(qs_pers, self).action_confirm()
 
     @api.depends('amount_total')
     def get_amount_letter(self):
