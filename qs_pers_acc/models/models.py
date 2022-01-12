6078e394ae769c935548ab37c5d61736a9917de1
AlphaConseils <alpha@alphamada.com>
2022-01-12 11:02:34 +0100

    To day push

--- qs_pers_acc/models/models.py
+++ qs_pers_acc/models/models.py
@@ -17,3 +17,25 @@ class qs_pers_acc(models.Model):
         related='partner_id.email',
         readonly=True)
 
+
+class AccountMoveLineInherited(models.Model):
+    _inherit = 'account.move.line'
+
+    @api.model
+    def get_pricelists(self):
+        """
+        Get pricelists which we want to display in product list
+        :return: dict : dictionary with pricelist id and name
+        """
+        pricelists = self.env['product.pricelist'].search([])
+        result = list()
+        for pricelist in pricelists:
+            values = {'id': pricelist.id, 'name': pricelist.name, 'display': pricelist.display_in_products}
+            result.append(values)
+        print(result)
+        return result
+
+
+class ProductTemplateInherit(models.Model):
+    _inherit = 'product.template'
+    transport = fields.Float(string="Transport")
