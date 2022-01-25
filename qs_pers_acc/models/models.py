9e1d6c30249f064f1ae8705fb13ff603cd8fb3a9
AlphaConseils <alpha@alphamada.com>
2022-01-25 17:07:32 +0100

    FIX[qs_acc] : margin analysis on price unit plus tranport

--- qs_pers_acc/models/models.py
+++ qs_pers_acc/models/models.py
@@ -25,25 +25,29 @@ class qs_pers_acc(models.Model):
         related='partner_id.email',
         readonly=True)
 
-    # def list_pricelist(self):
-    #     for record in self:
-    #         pricelist = self.env['product.pricelist'].search([('name', '=', 'PV GROS')])
-    #         print(pricelist)
-    #         if pricelist:
-    #             for rec in pricelist.item_ids:
-    #                 print(rec.product_tmpl_id.id)
-
-                # if self.product_id.id = record:
-
-            # print(pricelist.name)
-            # price_dict = pricelist.price_get(self.product_id.id, 1)
-            # print(price_dict)
-            # price = price_dict.get(pricelist.id)
-            # record.pv_det = price
-
 
 class AccountMoveLineInherited(models.Model):
     _inherit = 'account.move.line'
+    price_unit_qs = fields.Float(string="P.A", compute='_get_price_unii_qs')
+
+    @api.depends('price_unit')
+    def _get_price_unii_qs(self):
+        for record in self:
+            if record.price_unit:
+                record.price_unit_qs = record.price_unit
+            else:
+                record.price_unit_qs = 0
+
+    transport = fields.Float(string="Transport", compute='_get_transport')
+
+    @api.depends('product_id')
+    def _get_transport(self):
+        for record in self:
+            if record.product_id.transport:
+                record.transport = record.product_id.transport
+            else:
+                record.transport = 0
+
     pv_det = fields.Float(string='PV DET', compute='_get_pv_det', readonly=0, index=True)
     pv_det_marge = fields.Float(string='% PV DET', compute='_get_marge_pv_det')
 
@@ -65,9 +69,9 @@ class AccountMoveLineInherited(models.Model):
     @api.depends('pv_det')
     def _get_marge_pv_det(self):
         for record in self:
-            if record.product_id.standard_price != 0 and record.pv_det != 0:
-                denom = float(record.product_id.standard_price)
-                num = (record.pv_det - record.product_id.standard_price)
+            if record.price_unit_qs != 0 and record.pv_det != 0:
+                denom = float(record.price_unit_qs)
+                num = (record.pv_det - record.price_unit_qs)
                 record.pv_det_marge = (num / denom)
             else:
                 record.pv_det_marge = 0
@@ -93,9 +97,9 @@ class AccountMoveLineInherited(models.Model):
     @api.depends('pv_gros')
     def _get_marge_pv_gros(self):
         for record in self:
-            if record.product_id.standard_price != 0 and record.pv_gros != 0:
-                denom = float(record.product_id.standard_price)
-                num = (record.pv_gros - record.product_id.standard_price)
+            if record.price_unit_qs != 0 and record.pv_gros != 0:
+                denom = float(record.price_unit_qs)
+                num = (record.pv_gros - record.price_unit_qs)
                 record.pv_gros_marge = (num / denom)
             else:
                 record.pv_gros_marge = 0
@@ -121,9 +125,9 @@ class AccountMoveLineInherited(models.Model):
     @api.depends('pv_cr1')
     def _get_marge_pv_cr1(self):
         for record in self:
-            if record.product_id.standard_price != 0 and record.pv_cr1 != 0:
-                denom = float(record.product_id.standard_price)
-                num = (record.pv_cr1 - record.product_id.standard_price)
+            if record.price_unit_qs != 0 and record.pv_cr1 != 0:
+                denom = float(record.price_unit_qs)
+                num = (record.pv_cr1 - record.price_unit_qs)
                 record.pv_cr1_marge = (num / denom)
             else:
                 record.pv_cr1_marge = 0
@@ -149,9 +153,9 @@ class AccountMoveLineInherited(models.Model):
     @api.depends('pv_cr2')
     def _get_marge_pv_cr2(self):
         for record in self:
-            if record.product_id.standard_price != 0 and record.pv_cr2 != 0:
-                denom = float(record.product_id.standard_price)
-                num = (record.pv_cr2 - record.product_id.standard_price)
+            if record.price_unit_qs != 0 and record.pv_cr2 != 0:
+                denom = float(record.price_unit_qs)
+                num = (record.pv_cr2 - record.price_unit_qs)
                 record.pv_cr2_marge = (num / denom)
             else:
                 record.pv_cr2_marge = 0
@@ -170,6 +174,7 @@ class AccountMoveLineInherited(models.Model):
                         record.pv_cr3 = rec.fixed_price
                         break
                     else:
+
                         record.pv_cr3 = 0
             else:
                 record.pv_cr3 = 0
@@ -177,9 +182,9 @@ class AccountMoveLineInherited(models.Model):
     @api.depends('pv_cr3')
     def _get_marge_pv_cr3(self):
         for record in self:
-            if record.product_id.standard_price != 0 and record.pv_cr3 != 0:
-                denom = float(record.product_id.standard_price)
-                num = (record.pv_cr3 - record.product_id.standard_price)
+            if record.price_unit_qs != 0 and record.pv_cr3 != 0:
+                denom = float(record.price_unit_qs)
+                num = (record.pv_cr3 - record.price_unit_qs)
                 record.pv_cr3_marge = (num / denom)
             else:
                 record.pv_cr3_marge = 0
@@ -205,9 +210,9 @@ class AccountMoveLineInherited(models.Model):
     @api.depends('pv_cr4')
     def _get_marge_pv_cr4(self):
         for record in self:
-            if record.product_id.standard_price != 0 and record.pv_cr4 != 0:
-                denom = float(record.product_id.standard_price)
-                num = (record.pv_cr4 - record.product_id.standard_price)
+            if record.price_unit_qs != 0 and record.pv_cr4 != 0:
+                denom = float(record.price_unit_qs)
+                num = (record.pv_cr4 - record.price_unit_qs)
                 record.pv_cr4_marge = (num / denom)
             else:
                 record.pv_cr4_marge = 0
