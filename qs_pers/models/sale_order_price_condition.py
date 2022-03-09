90cc81b480c42e1ad1517bcd110d2f9727c96562
AlphaConseils <alpha@alphamada.com>
2022-03-09 17:00:54 +0100

    FIX:[qs_pers] : Add a price unit comparison revert

--- qs_pers/models/sale_order_price_condition.py
+++ qs_pers/models/sale_order_price_condition.py
@@ -5,6 +5,17 @@ from odoo.exceptions import UserError, Warning
 class SaleOrderLineInherit(models.Model):
     _inherit = 'sale.order.line'
 
+    # price_unit_comparison = fields.Boolean(compute="price_unit_comparison_func", default=False)
+    #
+    # def price_unit_comparison_func(self):
+    #     msg = "Le prix unitaire de certains articles n'est pas conforme (en ROUGE), veuillez appeler votre responsable pour changer les prix. "
+    #     for rec in self:
+    #         if rec.price_unit == 0:
+    #             raise UserError(msg)
+    #             rec.price_unit_comparison = True
+    #         else:
+    #             rec.price_unit_comparison = False
+
     @api.model
     def create(self, values):
         msg = "Le prix unitaire de certains articles n'est pas conforme (en ROUGE), veuillez appeler votre responsable pour changer les prix. "
