83b84b39503b127a7244e47f3c5d689f990e6fd9
AlphaConseils <alpha@alphamada.com>
2022-03-09 16:43:11 +0100

    FIX:[qs_pers] : Add a price unit comparison

--- qs_pers/models/sale_order_price_condition.py
+++ qs_pers/models/sale_order_price_condition.py
@@ -8,14 +8,14 @@ class SaleOrderLineInherit(models.Model):
     @api.model
     def create(self, values):
         msg = "Le prix unitaire de certains articles n'est pas conforme (en ROUGE), veuillez appeler votre responsable pour changer les prix. "
-
-        if self.price_unit == 0:
+        if self.price_unit != 0:
+            print("ato create true")
             raise UserError(msg)
-
         return super(SaleOrderLineInherit, self).create(values)
 
     def write(self, values):
         msg = "Le prix unitaire de certains articles n'est pas conforme (en ROUGE), veuillez appeler votre responsable pour changer les prix. "
-        if self.price_unit == 0:
+        if self.price_unit != 0:
+            print("ato write true")
             raise UserError(msg)
         return super(SaleOrderLineInherit, self).write(values)
