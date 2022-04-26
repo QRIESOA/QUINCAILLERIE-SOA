b020667e47cc071cb1e376575800cf3ad4b79a8b
AlphaConseils <alpha@alphamada.com>
2022-04-26 17:49:26 +0200

    FIX[qs_pers] :add  PV DET(MGA) on all product template view

--- qs_pers/models/product_template.py
+++ qs_pers/models/product_template.py
@@ -11,3 +11,20 @@ class ModelName(models.Model):
     repere = fields.Char(
         string='Repère',
         required=False)
+
+    detail_price = fields.Monetary(
+        string='PV DET(MGA)',
+        compute='_get_pv_det')
+
+    def _get_pv_det(self):
+        for record in self:
+            pv_det = 0
+            pricelist = record.env['product.pricelist.item'].search([
+                '|', ('product_tmpl_id', '=', record.id), ('product_id', 'in', record.product_variant_ids.ids)])
+            for rec in pricelist:
+                if rec.pricelist_id.display_name == 'PV DET (MGA)':
+                    pv_det = rec.fixed_price
+                    break
+                else:
+                    pv_det = 0
+            record.detail_price = pv_det
