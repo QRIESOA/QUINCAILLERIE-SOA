67689d2086ed89c0c38ca9521a880beb2d0c7786
Warren <nexources@nexources.com>
2023-01-05 20:25:46 +0300

    [INIT] qs_account: invoice report

---
+++ qs_account/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "SQ Account",
+    "version": "0.1",
+    "sequence": 100,
+    "category": "Account/Account",
+    "author": "Nexources",
+    "website": "https://www.nexources.com/",
+    "summary": "Track every user operation",
+    
+    "depends": [
+        'account',
+    ],
+    "data": [
+        # data
+
+        # security
+
+        # views
+        
+        # wizard
+
+        # report
+        'report/report_invoice.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
