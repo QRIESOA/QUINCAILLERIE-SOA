cd5b64e5d36b8b4d158a91ff2b2e92c805ca5017
Warren <nexources@nexources.com>
2023-01-13 14:56:48 +0300

    [FEAT] qs_account: is_charge field

--- qs_account/models/account_move.py
+++ qs_account/models/account_move.py
@@ -5,4 +5,5 @@ from odoo import models, fields
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    delivery_time = fields.Float(string='Delivery Time')
+    # delivery_time = fields.Float(string='Delivery Time')
+    is_charge = fields.Boolean()
