5e1a3ae7d5562f8398399b9b540edc3cc46f0351
Warren <warren@nexources.com>
2023-01-31 12:39:30 +0300

    [INIT] qs_sale

---
+++ qs_sale/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'QS Sales',
+    'version': '1.2',
+    'category': 'Sales/Sales',
+    'summary': 'Sales internal machinery',
+    'description': """
+This module contains all the common features of Sales Management and eCommerce.
+    """,
+    'depends': ['sale'],
+    'data': [
+    ],
+    'demo': [
+        'data/product_product_demo.xml',
+        'data/sale_demo.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'assets': {
+    },
+    'license': 'LGPL-3',
+}
