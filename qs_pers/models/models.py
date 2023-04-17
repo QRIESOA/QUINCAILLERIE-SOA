2e308862d87ffeb744d62e51e1c692d0b6fc54cf
Tokiniaina Rakotoarisoa <toky@nexources.com>
2023-04-17 11:21:36 +0300

    FEAT[qs_pers] change calcul of total_due for partenair to count the value of exlude invoice too

--- qs_pers/models/models.py
+++ qs_pers/models/models.py
@@ -190,8 +190,32 @@ class ResPartner(models.Model):
             if self.credit_limit < self.total_due:
                 raise UserError("La limite de crédit doit être supérieure aux factures impayées actuelles.")
 
-
-
+    @api.depends_context('company', 'allowed_company_ids')
+    def _compute_for_followup(self):
+        """
+        Compute the fields 'total_due', 'total_overdue','followup_level' and 'followup_status'
+        """
+        first_followup_level = self.env['account_followup.followup.line'].search([('company_id', '=', self.env.company.id)], order="delay asc", limit=1)
+        followup_data = self._query_followup_level()
+        today = fields.Date.context_today(self)
+        for record in self:
+            total_due = 0
+            total_overdue = 0
+            for aml in record.unreconciled_aml_ids:
+                if aml.company_id == self.env.company:
+                    amount = aml.amount_residual
+                    total_due += amount
+                    is_overdue = today > aml.date_maturity if aml.date_maturity else today > aml.date
+                    if is_overdue:
+                        total_overdue += amount
+            record.total_due = total_due
+            record.total_overdue = total_overdue
+            if record.id in followup_data:
+                record.followup_status = followup_data[record.id]['followup_status']
+                record.followup_level = self.env['account_followup.followup.line'].browse(followup_data[record.id]['followup_level']) or first_followup_level
+            else:
+                record.followup_status = 'no_action_needed'
+                record.followup_level = first_followup_level
 
     @api.depends('compute_field', 'user_id')
     def get_user(self):
