926515787eb6205830c1d18971059160a9f414cc
Tokiniaina Rakotoarisoa <toky@nexources.com>
2023-04-19 12:29:03 +0300

    FIX[qs_sale] fix readonly for pricelist field

--- qs_sale/models/sale_order.py
+++ qs_sale/models/sale_order.py
@@ -11,12 +11,13 @@ class SaleOrder(models.Model):
 
     partner_id = fields.Many2one("res.partner", default=lambda self: self.env.ref("qs_sale.client_comptant").id)
     note_client = fields.Char(string="Note")
-    can_change_list = fields.Boolean(string='Can create partenair', default=False, compute="_compute_can_create")
+    can_change_list = fields.Boolean(string='Can create partenair', default=lambda self: self.env.user.has_group("qs_sale.group_partner_change_list_so"), compute="_compute_can_create")
 
-    @api.depends("name")
+    # @api.depends()
     def _compute_can_create(self):
         self.can_change_list = self.env.user.has_group("qs_sale.group_partner_change_list_so")
 
+ 
     def _prepare_invoice(self):
         res = super(SaleOrder, self)._prepare_invoice()
         res['note_client'] = self.note_client
