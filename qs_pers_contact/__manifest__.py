69ef1a2548d485c37adf4988907ac09d9173a1ac
Tokiniaina Rakotoarisoa <toky@nexources.com>
2023-05-02 13:14:46 +0300

    HOTFIX[qs_account,qs_pers_contact]

--- qs_pers_contact/__manifest__.py
+++ qs_pers_contact/__manifest__.py
@@ -20,11 +20,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'sales_team', 'hr'],
+    'depends': ['base', 'sale', 'sales_team', 'hr', 'qs_pers_acc'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
+        'security/base_groups.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
