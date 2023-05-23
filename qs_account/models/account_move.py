7cd61b1519f484ca7c1b46f8706ad100b5ade33c
michael <michael@nexources.com>
2023-05-23 17:48:09 +0300

    [Hotfix]fix journal de paiment

--- qs_account/models/account_move.py
+++ qs_account/models/account_move.py
@@ -1,6 +1,7 @@
 #-*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from datetime import date, datetime, time
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
@@ -8,16 +9,30 @@ class AccountMove(models.Model):
     # delivery_time = fields.Float(string='Delivery Time')
     is_charge = fields.Boolean()
     is_spec = fields.Boolean(string="is_spec", compute="_compute_is_spec")
-    # journal_paie_name = fields.Char(string="payment journal", compute='journal_name_compute', store=True)
-    payment_paie_date = fields.Date(compute='journal_name_compute', store=True, string="payment date")
-    # communication = fields.Char(compute='journal_name_compute', store=True)
+    journal_paie_types = fields.Char(compute='journal_types_compute')
+    # payment_paie_date = fields.Date(compute='journal_name_compute', store=True, string="payment date")
+    payment_paie_date = fields.Date(compute='journal_name_compute')
+    # communication = fields.Char(compute='journal_name_compute', store=True)  self.env["account.payment"].search([('ref', '=', 'FAC/2023/01626')]).journal_id.name
     about_payment = fields.Char(compute='_compute_about_payment')
 
-    @api.depends('payment_id')
+
+    def journal_types_compute(self):
+        for rec in self:
+            rec.journal_paie_types = False
+            json_values = rec._get_reconciled_info_JSON_values()
+            if json_values:
+                rec.journal_paie_types = ', '.join(val.get('journal_name', '') for val in json_values)
+
+
     def journal_name_compute(self):
         for rec in self:
-            invoice_to_w = self.env["account.move"].search([('name', '=', rec.ref)])
-            invoice_to_w.write({"payment_paie_date": rec.payment_id.date})
+            rec.payment_paie_date = False
+            json_values = rec._get_reconciled_info_JSON_values()
+            if json_values:
+                rec.payment_paie_date = json_values[0]['date']
+
+            # invoice_to_w = self.env["account.move"].search([('name', '=', rec.ref)])
+            # invoice_to_w.write({"payment_paie_date": rec.payment_id.date})
 
 
     def _compute_about_payment(self):
