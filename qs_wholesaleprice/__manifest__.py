8f225e2660a85150249ea79fc82e8b43cf54cdab
NyAro-Nexources <nyaro@nexources.com>
2024-06-06 08:07:52 +0300

    [IMP] qs_wholesaleprice: Module initialization

---
+++ qs_wholesaleprice/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "QS - wholesaleprice",
+
+    'summary': "QS - wholesaleprice",
+
+    'author': "Nexources",
+    'website': "http://www.nexources.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base', 'product'],
+
+    'data': [
+        'views/product_template_views.xml',
+    ],
+}
