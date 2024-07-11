60fbd3a6c9d14a96a1fb1f642c77e0552d503b44
NyAro-Nexources <nyaro@nexources.com>
2024-07-11 13:57:38 +0300

    [FIX]: sale menu and button

---
+++ qs_sale_restrict/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "QS - Sale restrict",
+
+    'summary': "QS - Sale restrict",
+
+    'author': "Nexources",
+    'website': "http://www.nexources.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base', 'sale'],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+        'security/security.xml',
+        'views/views.xml',
+    ]
+}
