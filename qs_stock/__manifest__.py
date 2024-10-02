3bebf5cdc9d7399f2ef10949e9dbe69b8a6613b6
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 <michael@nexources>
2024-10-02 14:18:06 +0300

    [ADD] interchange columns

---
+++ qs_stock/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "qs_stock",
+    'author': "Nexources", 
+    'website': "http://www.nexources.com",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base','stock'],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+        "views/product_views.xml",
+    ],
+    
+}
