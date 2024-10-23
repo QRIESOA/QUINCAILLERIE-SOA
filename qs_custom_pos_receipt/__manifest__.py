f4aa6ccb5eb90ec8283aaaf64facb60b68b57435
NyAro-Nexources <nyaro@nexources.com>
2024-10-23 13:26:46 +0300

    [FIX]: Mise en page ticket et facture

---
+++ qs_custom_pos_receipt/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "QS - POS",
+    "summary": "Inherit POS congig and receipt",
+    "author": "Nexources",
+    "website": "http://www.nexources.com",
+    "category": "Uncategorized",
+    "version": "0.1",
+    "depends": ["base", "point_of_sale", "custom_pos_receipt"],
+    "data": [
+        "views/pos_config_view.xml",
+    ],
+}
